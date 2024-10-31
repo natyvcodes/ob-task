e6969a96e7cb41e9448f3bf132c00b5b6ec539e8
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-10-31 18:19:26 -0500

    added import modules for routing pages on header component

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css'
 })
