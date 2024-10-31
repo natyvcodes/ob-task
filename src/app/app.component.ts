1a00e7d3769234d4987f57f20b58902a41a744fc
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-10-31 18:26:15 -0500

    Imported components, and modules for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, RouterLink, RouterLinkActive, AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ob-tasks-app';
-}
+export class AppComponent {}
