d0c249cf43c36d388a0dfb367fae4060befffd04
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-12-02 14:50:15 -0500

    Tasks page styles added

--- src/app/my-tasks/my-tasks.component.ts
+++ src/app/my-tasks/my-tasks.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-my-tasks',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatTooltipModule],
   templateUrl: './my-tasks.component.html',
   styleUrl: './my-tasks.component.css'
 })
