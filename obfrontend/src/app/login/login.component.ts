37485d57e4f24f3464e8b25a8e4fdf7cf3a00cfc
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-12-07 14:03:08 -0500

    New dialog component added, fixed image width

--- obfrontend/src/app/login/login.component.ts
+++ obfrontend/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import { MatDialogModule} from '@angular/material/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
--- obfrontend/src/app/my-tasks/my-tasks.component.ts
+++ obfrontend/src/app/my-tasks/my-tasks.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { AddtaskComponent } from '../addtask/addtask.component';
 
 @Component({
   selector: 'app-my-tasks',
@@ -11,5 +13,13 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class MyTasksComponent {
     existTasks: boolean = false;
-    
+    readonly dialog = inject(MatDialog);
+    showTaskRegister(){
+    const dialogRef = this.dialog.open(AddtaskComponent,{
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
