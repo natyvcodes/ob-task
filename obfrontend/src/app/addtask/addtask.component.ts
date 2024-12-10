e265cbef1dc50eed45453ad85c305228fb0669d2
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-12-10 13:00:36 -0500

    Form modified to send information with the user id

--- obfrontend/src/app/addtask/addtask.component.ts
+++ obfrontend/src/app/addtask/addtask.component.ts
@@ -2,14 +2,22 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ApiService } from '../api.service';
 import { catchError, map } from 'rxjs/operators';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { forkJoin, of } from 'rxjs';
 import { TaskAddedDialogComponent } from '../task-added-dialog/task-added-dialog.component';
 import { AuthService } from '../auth.service';
-
+import { MatDialogRef } from '@angular/material/dialog';
+interface Task {
+  id: string;
+  name: string;
+  description: string;
+  user_id: string;
+  id_state: string;
+  id_category: string;
+}
 @Component({
   selector: 'app-addtask',
   standalone: true,
@@ -27,8 +35,10 @@ export class AddtaskComponent implements OnInit {
   id_s: number[] = []
   myForm: FormGroup;
   formSend: boolean = false;
+  userId: string | null  = '';
+  taskData: Task[] = []
 
-  constructor(private apiService: ApiService, private formBuilder: FormBuilder, private authService: AuthService) {
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder, private dialogRef: MatDialogRef<AddtaskComponent>) {
     this.myForm = this.formBuilder.group({
       user_id: ['', Validators.required],
       name: ['', Validators.required],
@@ -39,44 +49,46 @@ export class AddtaskComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      forkJoin({
-        categories: this.apiService.getCategories().pipe(
-          map(data => {
-            this.category = data;
-            this.name_category = this.category.map(c => c.description)
-            this.id_c = this.category.map(c => c.id)
-          }),
-          catchError(error => {
-            console.error('An error occurred:', error)
-            return of('Error occurred')
-          })
-        ),
-        states: this.apiService.getStates().pipe(
-          map(data => {
-            this.state = data;
-            this.name_state = this.state.map(c => c.description)
-            this.id_s= this.state.map(c => c.id)
-          }),
-          catchError(error => {
-            console.error('An error occurred:', error)
-            return of('Error occurred')
-          })
-        )
-      }).subscribe()
-     
-    
+    forkJoin({
+      categories: this.apiService.getCategories().pipe(
+        map(data => {
+          this.category = data;
+          this.name_category = this.category.map(c => c.description)
+          this.id_c = this.category.map(c => c.id)
+        }),
+        catchError(error => {
+          console.error('An error occurred:', error)
+          return of('Error occurred')
+        })
+      ),
+      states: this.apiService.getStates().pipe(
+        map(data => {
+          this.state = data;
+          this.name_state = this.state.map(c => c.description)
+          this.id_s = this.state.map(c => c.id)
+        }),
+        catchError(error => {
+          console.error('An error occurred:', error)
+          return of('Error occurred')
+        })
+      )
+    }).subscribe()
+
   }
   submitForm() {
-    for(let i = 0; i < this.name_state.length; i++){
-      if(this.name_state[i] == this.myForm.get('id_state')?.value){
+    for (let i = 0; i < this.name_state.length; i++) {
+      if (this.name_state[i] == this.myForm.get('id_state')?.value) {
         this.myForm.get('id_state')?.setValue(this.id_s[i]);
       }
     }
-    for(let i = 0; i < this.category.length; i++){
-      if(this.name_category[i] === this.myForm.get('id_category')?.value){
+    for (let i = 0; i < this.category.length; i++) {
+      if (this.name_category[i] === this.myForm.get('id_category')?.value) {
         this.myForm.get('id_category')?.setValue(this.id_c[i]);
       }
     }
+    this.userId = localStorage.getItem('userId');
+    this.myForm.get('user_id')?.setValue(this.userId)
+
     if (this.myForm.valid) {
       const data = this.myForm.value;
       this.apiService.createTask(data).subscribe({
@@ -84,25 +96,22 @@ export class AddtaskComponent implements OnInit {
           this.myForm.reset();
           this.formSend = true;
           this.taskAdded(this.formSend);
+          this.dialogRef.close(response);
         },
         error: error => {
           alert('Error al añadir la tarea: ' + error.message);
         }
       }
       );
-    }else {
+    } else {
       alert('completa todos los campos')
     }
-    
+
   }
-  taskAdded(confirm: boolean){
-    if(confirm){
-      const dialogRef = this.dialog.open(TaskAddedDialogComponent,{
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log(`Dialog result: ${result}`);
-      });
+  taskAdded(confirm: boolean) {
+    if (confirm) {
+      const dialogRef = this.dialog.open(TaskAddedDialogComponent);
     }
-  } 
+
+  }
 }
