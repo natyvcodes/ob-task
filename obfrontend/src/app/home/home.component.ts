4e63b1856e23229b1370cfc5efba78d158a2447f
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-12-12 15:42:02 -0500

    Recovering lose files

---
+++ obfrontend/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { RegisterComponent } from '../register/register.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ MatDialogModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+  readonly dialog = inject(MatDialog);
+  showLogin(){
+    const dialogRef = this.dialog.open(RegisterComponent,{
+      width: '360px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
