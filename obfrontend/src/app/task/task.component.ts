524696a6f7882803658002379a5576548d02c817
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-01-18 14:05:21 -0500

    New input to tranfer task data added

--- obfrontend/src/app/task/task.component.ts
+++ obfrontend/src/app/task/task.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { Component, inject, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -9,8 +12,18 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 })
 export class TaskComponent {
   @Input() taskName!: string;
+  @Input() taskId!: string;
+  readonly dialog = inject(MatDialog)
+  constructor(private apiService: ApiService) { }
+  deleteTask(taskId: string) {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '300px'
+    })
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == 1) {
+        this.apiService.deleteTask(taskId).subscribe()
+      }
+    })
 
-  deleteTask(){
-    
   }
 }
