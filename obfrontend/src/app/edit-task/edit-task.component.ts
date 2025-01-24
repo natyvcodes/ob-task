42805ace5e75d39b64d42c9a40269b030a2dfd1a
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-01-24 11:28:24 -0500

    Edit task reactive form added

--- obfrontend/src/app/edit-task/edit-task.component.ts
+++ obfrontend/src/app/edit-task/edit-task.component.ts
@@ -6,46 +6,70 @@ import { ViewChild } from '@angular/core';
 import { ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { UpdateTask } from '../api.service';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-edit-task',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule,MatDialogModule],
+  imports: [ReactiveFormsModule, FormsModule, MatDialogModule],
   templateUrl: './edit-task.component.html',
   styleUrl: './edit-task.component.css'
 })
 export class EditTaskComponent {
   @ViewChild('textareaRef') textareaRef!: ElementRef<HTMLTextAreaElement>;
   taskInfo!: Task;
-  category: string[] = ["","Urgent", "Important", "Not Urgent"]
+  category: string[] = ["", "Urgent", "Important", "Not Urgent"]
+  state: string[] = ["", "Pending", "In Process", "Complete"]
   editOpen: boolean = true;
   EditForm: FormGroup;
+  isLoggedIn: boolean = false;
+  taskUpdated!: UpdateTask;
 
-
-  constructor(private formBuilder: FormBuilder,@Inject(MAT_DIALOG_DATA) data: { taskInfo: Task }, private dialogRef: MatDialogRef<EditTaskComponent>) {
+  constructor(private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) data: { taskInfo: Task },
+    private dialogRef: MatDialogRef<EditTaskComponent>,
+    private authService: AuthService,
+    private apiService: ApiService
+  ) {
     this.taskInfo = data.taskInfo
-    this.EditForm =  this.formBuilder.group({
-
+    this.EditForm = this.formBuilder.group({
+      name: [this.taskInfo.name, Validators.required],
+      description: [this.taskInfo.description, Validators.required],
+      id_category: [this.taskInfo.id_category, Validators.required],
+      id_state: [this.taskInfo.id_state, Validators.required],
+      id: [this.taskInfo.id, Validators.required]
     })
   }
-  submitForm(){
+  submitForm() {
+    this.authService.userLoggedInC.subscribe((isloggedIn: boolean) => {
+      this.isLoggedIn = isloggedIn;
+    })
+    if (this.EditForm.valid && this.isLoggedIn) {
+      this.taskUpdated = this.EditForm.value;
+      this.apiService.updateTask(this.taskUpdated).subscribe()
 
+    }
   }
-  setCategory(id_category: number): string {
-      const category: string = this.category[id_category]
-      return category
+  getCategories(): { index: number, value: string }[] {
+    return this.category
+      .map((value, index) => ({ index, value }))
+      .filter((item, index, self) => item.index !== 0 && self.findIndex(t => t.index === item.index) === index);  // Asegura índices únicos
   }
-  getRemainingCategories(selectedId: number): string[] {
-    return this.category.filter((_, index) => index !== selectedId && index > 0); // Excluimos el índice `0` vacío
+  getStates(): { index: number, value: string }[] {
+    return this.state
+      .map((value, index) => ({ index, value }))
+      .filter((item, index, self) => item.index !== 0 && self.findIndex(t => t.index === item.index) === index);  // Asegura índices únicos
   }
+  
   closeDialog(taskId: string): void {
     this.dialogRef.close(taskId);
   }
   editInfo() {
     this.editOpen = !this.editOpen;
-    console.log(this.editOpen)
-    if(!this.editOpen) {
-        this.textareaRef.nativeElement.focus();
+    if (!this.editOpen) {
+      this.textareaRef.nativeElement.focus();
     }
   }
 }
