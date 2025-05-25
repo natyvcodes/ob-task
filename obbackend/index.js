77363ac95f30c856cc70e7a2d119b63719fc172f
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-24 20:48:27 -0500

    final fix

---
+++ obbackend/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const db = require('./src/queries');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'https://obtask.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+app.options('*', cors());
+app.get('/', (req, res) => {
+    res.json({ message: 'Hola' });
+
+})
+app.post('/addTask', db.addTask)
+app.get('/categories', db.getCategories)
+app.get('/states', db.getStates)
+app.post('/login', db.authUser)
+app.post('/userTasks', db.getUserTasks)
+app.post('/registerUser', db.registerUser)
+app.post('/deleteTask', db.deleteTask)
+app.post('/updateTask', db.updateTask)
+app.post('/deleteUser', db.deleteUser)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app;
