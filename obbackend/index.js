eca8599213959d60c58128d537785f4cd64fe801
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2025-05-24 19:41:17 -0500

    change allow cors url

--- obbackend/index.js
+++ obbackend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: process.env.ALLOW_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
